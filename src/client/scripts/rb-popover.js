2838b8679dd977e7b8cd9fd15507d81f6d8c351c
yokun <yevgeniyokun@gmail.com>
2018-03-02 02:15:03 -0600

    feat(position): add position for left and right relative to the window

--- src/client/scripts/rb-popover.js
+++ src/client/scripts/rb-popover.js
@@ -17,7 +17,6 @@ export class RbPopover extends PolymerElement {
 		this.popoverElm = this.root.querySelector('.popover')
 		this.pointerElm = this.root.querySelector('.pointer')
 		this.triggerElm = this.root.querySelector('rb-icon')
-
 	}
 
 	/* Properties
@@ -54,9 +53,9 @@ export class RbPopover extends PolymerElement {
 	/* Event Handlers
 	 *****************/
 	_handleClick(e) { // :void
-		this._show = !this._show;
-		this._setPosition(this.position)
 		e.preventDefault();
+		this._show = !this._show;
+		this._adjustToWindow();
 	}
 
 	_handleHover(e) {
@@ -64,52 +63,73 @@ export class RbPopover extends PolymerElement {
 			this._show = true;
 	}
 
-	_setPosition(position, e) {
-		setTimeout(() => {
-			switch(position) {
-				case 'top':
-				this._setTopPosition()
-					break;
-				case 'bottom':
-					this._setBottomPosition()
-					break;
-				case 'left':
-					this._setLeftPosition()
-					break
-				default:
-					this._setRightPosition()
-			}
-
-		}, 0);
-
-		return {}
-	}
-
 	_adjustToWindow() {
-		console.log(window.scrollY)
-		console.log(window.screenLeft)
+		if (!this._show) {
+			return;
+		}
+
+		var winWidth = window.innerWidth;
+		var triggerX = this.getBoundingClientRect().left;
+		var popoverX = this.popoverElm.getBoundingClientRect().left;
+		var popoverWidth = this.popoverElm.offsetWidth;
+
+		console.log(winWidth, popoverX,  triggerX)
+
+		switch (true) {
+			case ((triggerX - popoverWidth - 9) < 0 || popoverX < 0):
+				console.log(1)
+				this._setRightPosition();
+				break;
+			case (triggerX + popoverWidth + 9 > winWidth):
+				console.log(2)
+				this._setLeftPosition();
+				break;
+			default:
+				console.log(3)
+				this._setPosition();
+		}
 
 	}
 
+	_setPosition() {
+		switch (this.position) {
+			case 'left':
+				this._setLeftPosition();
+				break;
+			case 'top':
+				this._setTopPosition();
+				break;
+			case 'bottom':
+				this._setBottomPosition();
+				break;
+			default:
+				this._setRightPosition()
+		}
+	}
 
 	_setTopPosition() {
+		this._position = 'top';
 		this.popoverElm.style.left = this.triggerElm.offsetLeft - (this.popoverElm.offsetWidth/2 - 8) + 'px'
 		this.popoverElm.style.top =  this.triggerElm.offsetTop - (this.pointerElm.offsetHeight + this.popoverElm.offsetHeight + 2) + 'px'
-		// console.log(this.popoverElm.offsetWidth)
-		// this._adjustToWindow()
 	}
 
 	_setBottomPosition() {
+		this._position = 'bottom';
 		this.popoverElm.style.left = this.triggerElm.offsetLeft - (this.popoverElm.offsetWidth/2 - 8) + 'px'
 		this.popoverElm.style.top =  this.offsetHeight + this.pointerElm.offsetHeight + 2 + 'px'
 	}
 	_setLeftPosition() {
+		console.log('left')
+		this._position = 'left';
 		this._setRightLeftPositionTop();
 		this.popoverElm.style.left = this.triggerElm.offsetLeft - this.popoverElm.offsetWidth - this.pointerElm.offsetWidth - 2 + 'px'
 	}
 
 	_setRightPosition() {
+		console.log('right')
+		this._position = 'right';
 		this._setRightLeftPositionTop();
+		this.popoverElm.style.left = this.triggerElm.offsetLeft + this.pointerElm.offsetWidth + this.triggerElm.offsetWidth + 2 +'px'
 	}
 
 	_setRightLeftPositionTop() {
@@ -121,6 +141,7 @@ export class RbPopover extends PolymerElement {
 			}
 
 		}
+
 	}
 
 	_showPopover(show) {
